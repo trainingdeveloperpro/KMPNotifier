90ccbd5a0fe2bb0568ae19c77b0a1fd6cc68fd34
Linh Van Nguyen <linhvnguyen9@gmail.com>
2024-08-02 17:39:23 +0700

    Change plugins declaration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-    alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidLibrary)
-    alias(libs.plugins.kotlinNativeCocoaPods)
+    kotlin("multiplatform")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.native.cocoapods")
 
 }
 
